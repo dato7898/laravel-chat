5d6ede252faa3877eec8ea0d2e755c479036452a
David Dolgov <davidd7598@gmail.com>
2020-08-08 23:35:38 +0600

    Update database connection

--- config/database.php
+++ config/database.php
@@ -14,8 +14,11 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
+    
+    $DATABASE_URL=parse_url('postgres://rcyhrrupycdnss:4b6e99be87cfc7eb3b778a9ef96f818527cf7e9fad56be9701e72ddd76195482@ec2-52-203-160-194.compute-1.amazonaws.com:5432/ddeaat9eq959pf
+');
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +68,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
